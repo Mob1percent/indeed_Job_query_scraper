3a4b5344fb43a946b477e97807dd95526fcd58b9
jun wang <92771981+Mob1percent@users.noreply.github.com>
2022-03-21 17:40:39 +0000

    Final

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+keys = ['co-op','intern','internship','coop']
+keys2=['software','computer','developer']
 ## needed info (Comany,Discipline,City,Job_title,Level,Link)
 def extract(type,city,page):
   headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
@@ -11,19 +12,32 @@ def extract(type,city,page):
   return soup
 
 def transform(soup):
-  discipline = 'software' #planning to write a if-else statement that read title and check if it said 'software developer etc'-software discipline or 'power engnieer etc' - hardware discipline
-  level = 'full-time'  #planning to write a if-else statement that read title and check if it said 'coop' or 'intern' to declare it as intern-level
+   #planning to write a if-else statement that read title and check if it said 'software developer etc'-software discipline or 'power engnieer etc' - hardware discipline
+  #level = 'full-time'  #planning to write a if-else statement that read title and check if it said 'coop' or 'intern' to declare it as intern-level
+  for EachPart in soup.select('a[class*="tapItem fs-unmask result job"]'):
+      (EachPart.get_text())
+      part=EachPart['href']
+      url='ca.indeed.com'+part
   divs = soup.find_all('div', class_ = 'job_seen_beacon')
   for item in divs:
     title = item.find('h2').text
     company = item.find('span',class_ ='companyName').text
     companylocation = item.find('div',class_ = 'companyLocation').text
+    if (any(key in title.lower() for key in keys)):
+        level = 'intern'
+    else:
+        level = 'Full-time'
+    if(any(key in title.lower() for key in keys2)):
+        discipline = 'software'
+    else:
+        discipline ='hardware'
     job ={
       'company':company,
       'Discipline':discipline,
       'location':companylocation,
       'title':title,
-      'level':level
+      'level':level,
+      'URL':url
     }
     joblist.append(job)
   return
@@ -36,6 +50,6 @@ def transform(soup):
 
 joblist =[]
 
-c = extract('software%20developer','toronto', 0)
+c = extract('software%20developer%20intern','toronto', 0)
 transform(c)
 print(joblist)
